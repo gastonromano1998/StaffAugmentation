36b0304ed50ff4ad2f21443fd311bbc79a41aaf7
gastonromano <gaastonromano@gmail.com>
2021-11-08 19:53:27 -0300

    cambios finales

--- js/contact.js
+++ js/contact.js
@@ -7,7 +7,7 @@ function handleSubmit(event) {
     event.preventDefault()
     const form = new FormData(this)
     console.log(form.get('name'))
-    $buttonMailto.setAttribute('href', `mailto:gaastonromano@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
+    $buttonMailto.setAttribute('href', `mailto:cdr@dynamicsteams.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
     $buttonMailto.click()
 }
 
